88a35a677307c485fb6fb6277fef50f5847f3edf
root <root@ng6c13aaa-master-instance-lln6bwaw.novalocal>
2017-03-21 20:18:07 +0800

    backup

--- preprocess/download.sh
+++ preprocess/download.sh
@@ -3,7 +3,7 @@
 id=$1
 
 rm -rf /mnt/download
-mkdir -p download
+mkdir -p /mnt/download
 cd /mnt/download
 wget http://roadhack-sources.bj.bcebos.com/train/image/"$id".zip -O image.zip &
 wget http://roadhack-sources.bj.bcebos.com/train/attr/"$id".zip -O attr.zip &
